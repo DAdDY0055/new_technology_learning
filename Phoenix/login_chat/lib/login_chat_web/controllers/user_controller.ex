9ba69bb62206ee4554de1987aa2c5ad0ca9c723b
Takuya <yky2800.rthm.55tn@gmail.com>
2019-07-21 17:04:34 +0900

    コントローラー作成

---
+++ Phoenix/login_chat/lib/login_chat_web/controllers/user_controller.ex
@@ -0,0 +1,9 @@
+defmodule LoginChatWeb.UserController do
+  use LoginChatWeb, :controller
+  alias LoginChat.User
+
+  def index(conn, _params) do
+    users = Repo.all(User)
+    render(conn, "index.html", users: users)
+  end
+end
