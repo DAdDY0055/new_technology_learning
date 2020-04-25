f9daccf795a4721726a19a51d353e72ea4fe37cb
Takuya <yky2800.rthm.55tn@gmail.com>
2020-04-25 23:36:01 +0900

    Todoリスト作成復習

--- vuejs/todo2/js/main.js
+++ vuejs/todo2/js/main.js
@@ -1,6 +1,22 @@
-new Vue({
+var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js'
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function (event) {
+      if(this.newItem == '') return 
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      }
+      this.todos.push(todo)
+      this.newItem = ""
+    },
+    deleteItem: function(index) {
+      // alert(index);
+      this.todos.splice(index, 1)
+    }
   }
 })
