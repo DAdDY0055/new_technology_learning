e56bced2821073000a5bd4ca2c6a9b750d408c9a
Takuya <yky2800.rthm.55tn@gmail.com>
2020-05-04 20:51:51 +0900

    TypeScript入門 #8-9 Promiseによる非同期通信

---
+++ typescript/#8/sample08.js
@@ -0,0 +1,22 @@
+"use strict";
+const zipSync = new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'zip.json', true);
+    console.log(xhr.response);
+    xhr.onload = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                // console.log(xhr.response);
+                resolve(xhr.response);
+            }
+            else {
+                reject(new Error(xhr.statusText));
+            }
+        }
+    };
+    xhr.send(null);
+}).then((value) => {
+    console.log(value);
+}).catch((resson) => {
+    console.log(resson);
+});
