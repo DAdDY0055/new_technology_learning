092b653968a8dfd36ec4581383490eee01a89218
Takuya <yky2800.rthm.55tn@gmail.com>
2021-02-13 00:31:42 +0900

    プログラミング言語Go 4.3.1 ResponseWriterへの書き込み 4.11 JSON出力の書き込み

--- go/programing_go/src/chapter4/4.3.1/main.go
+++ go/programing_go/src/chapter4/4.3.1/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+// Post 投稿の型
+type Post struct {
+	User    string
+	Threads []string
+}
 
 func writerExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html><head><title>Go Web Programing</title></head><body><h1>Hello World</h1></body></hmtl>`
@@ -20,6 +27,16 @@ func handerExample(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(302)
 }
 
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User: "Taku",
+		Threads: []string{"1ばんめ", "2ばんめ", "3ばんめ"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
@@ -27,5 +44,6 @@ func main() {
 	http.HandleFunc("/write", writerExample)
 	http.HandleFunc("/writeheader", writerHeaderExample)
 	http.HandleFunc("/redirect", handerExample)
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
 }
