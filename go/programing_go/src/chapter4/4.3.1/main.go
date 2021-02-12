63c52da758b355b0298948138ca6eecd979f6e1a
Takuya <yky2800.rthm.55tn@gmail.com>
2021-02-12 23:48:11 +0900

    プログラミング言語Go 4.3.1 ResponseWriterへの書き込み 4.9 WriteHeaderによるレスポンスヘッダの書き込み

--- go/programing_go/src/chapter4/4.3.1/main.go
+++ go/programing_go/src/chapter4/4.3.1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -9,10 +10,16 @@ func writerExample(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(str))
 }
 
+func writerHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "そのようなサービスはありません。他を利用してください。")
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/write", writerExample)
+	http.HandleFunc("/writeheader", writerHeaderExample)
 	server.ListenAndServe()
 }
