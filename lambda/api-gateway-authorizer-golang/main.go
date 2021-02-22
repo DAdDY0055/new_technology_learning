8991a0a9f3fbd6a8c952e793b837790d77e5dd3f
Takuya <yky2800.rthm.55tn@gmail.com>
2021-02-22 20:43:25 +0900

    JWTトークンを受け取ってデコードするLambda関数を作成

--- lambda/api-gateway-authorizer-golang/main.go
+++ lambda/api-gateway-authorizer-golang/main.go
@@ -2,171 +2,75 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/k-washi/jwt-decode/jwtdecode"
 )
 
-func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	log.Println("Client token: " + event.AuthorizationToken)
-	log.Println("Method ARN: " + event.MethodArn) // Amazon Resource Names
+// Help function to generate an IAM policy
+func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
 
-	token := event.AuthorizationToken
-	principalID := "user|a1b2c3d4" // TODO: これは？本来受け取るもの？
-
-	tmp := strings.Split(event.MethodArn, ":")
-	apiGatewayArnTmp := strings.Split(tmp[5], "/")
-	log.Println("tmp: ", tmp)
-	log.Println("apiGatewayArnTmp: ", apiGatewayArnTmp)
-	awsAccountID := tmp[4]
-
-	resp := NewAuthorizerResponse(principalID, awsAccountID)
-	resp.Region = tmp[3]
-	resp.APIID = apiGatewayArnTmp[0]
-	resp.Stage = apiGatewayArnTmp[1]
-
-	switch token {
-	case "admin":
-		resp.AllowAllMethods()
-	case "user":
-		resp.AllowMethod(Get, "/user/*")
-	default:
-		resp.DenyAllMethods()
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
 	}
 
+	// Optional output with custom properties of the String, Number or Boolean type.
+	// authResponse.Context = map[string]interface{}{
+	// 	"stringKey":  "stringval",
+	// 	"numberKey":  123,
+	// 	"booleanKey": true,
+	// }
+	return authResponse
+}
 
-	return resp.APIGatewayCustomAuthorizerResponse, nil
+func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	token := event.AuthorizationToken
+
+	//JWTをheader, claims, 署名に分解する。
+	hCS, err := jwtdecode.JwtDecode.DecomposeFB(token)
+	if err != nil {
+			log.Fatalln("Error : ", err)
+	}
+	fmt.Println("hCS", hCS)
+
+	payload, err := jwtdecode.JwtDecode.DecodeClaimFB(hCS[1])
+	if err != nil {
+			log.Fatalln("Error :", err)
+	}
+	fmt.Println("payload", payload)
+
+	//ユーザーIDと, メールアドレスを表示
+	user := payload.Subject
+	email := payload.Email
+	fmt.Printf("User ID: " + user + " ,Email: " + email + "\n")
+
+	switch strings.ToLower(token) {
+	case "allow":
+		return generatePolicy("user", "Allow", event.MethodArn), nil
+	case "deny":
+		return generatePolicy("user", "Deny", event.MethodArn), nil
+	case "unauthorized":
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
+	default:
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+	}
 }
 
 func main() {
 	lambda.Start(handleRequest)
 }
-
-type HttpVerb int
-
-const (
-	Get HttpVerb = iota
-	Post
-	Put
-	Delete
-	Patch
-	Head
-	Options
-	All
-)
-
-func (hv HttpVerb) String() string {
-	switch hv {
-	case Get:
-		return "GET"
-	case Post:
-		return "POST"
-	case Put:
-		return "PUT"
-	case Delete:
-		return "DELETE"
-	case Patch:
-		return "PATCH"
-	case Head:
-		return "HEAD"
-	case Options:
-		return "OPTIONS"
-	case All:
-		return "*"
-	}
-	return ""
-}
-
-type Effect int
-
-const (
-	Allow Effect = iota
-	Deny
-)
-
-func (e Effect) String() string {
-	switch e {
-	case Allow:
-		return "Allow"
-	case Deny:
-		return "Deny"
-	}
-	return ""
-}
-
-type AuthorizerResponse struct {
-	events.APIGatewayCustomAuthorizerResponse
-
-	// The region where the API is deployed. By default this is set to '*'
-	Region string
-
-	// The AWS account id the policy will be generated for. This is used to create the method ARNs.
-	AccountID string
-
-	// The API Gateway API id. By default this is set to '*'
-	APIID string
-
-	// The name of the stage used in the policy. By default this is set to '*'
-	Stage string
-}
-
-func NewAuthorizerResponse(principalID string, AccountID string) *AuthorizerResponse {
-	return &AuthorizerResponse{
-		APIGatewayCustomAuthorizerResponse: events.APIGatewayCustomAuthorizerResponse{
-			PrincipalID: principalID,
-			PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-				Version: "2012-10-17", // バージョンはこれでないとInvalid versionになる
-			},
-		},
-		Region:    "*",
-		AccountID: AccountID,
-		APIID:     "*",
-		Stage:     "*",
-	}
-}
-
-func (r *AuthorizerResponse) addMethod(effect Effect, verb HttpVerb, resource string) {
-	resourceArn := "arn:aws:execute-api:" +
-		r.Region + ":" +
-		r.AccountID + ":" +
-		r.APIID + "/" +
-		r.Stage + "/" +
-		verb.String() + "/" +
-		strings.TrimLeft(resource, "/")
-
-	s := events.IAMPolicyStatement{
-		Effect:   effect.String(),
-		Action:   []string{"execute-api:Invoke"},
-		Resource: []string{resourceArn},
-	}
-
-	r.PolicyDocument.Statement = append(r.PolicyDocument.Statement, s)
-}
-
-func checkAuthority(token string) string {
-	switch token {
-	case "allow":
-		return "Allow"
-	case "deny":
-		return "Deny"
-	}
-	return ""
-}
-
-func (r *AuthorizerResponse) AllowAllMethods() {
-	r.addMethod(Allow, All, "*")
-}
-
-func (r *AuthorizerResponse) DenyAllMethods() {
-	r.addMethod(Deny, All, "*")
-}
-
-func (r *AuthorizerResponse) AllowMethod(verb HttpVerb, resource string) {
-	r.addMethod(Allow, verb, resource)
-}
-
-// func (r *AuthorizerResponse) DenyMethod(verb HttpVerb, resource string) {
-// 	r.addMethod(Deny, verb, resource)
-// }
