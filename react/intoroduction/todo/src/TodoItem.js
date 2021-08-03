46feeb3f1029bd1ccd28225e88e5e90e065cc173
Takuya <yky2800.rthm.55tn@gmail.com>
2021-08-03 22:02:03 +0900

    feat: formの操作

--- react/intoroduction/todo/src/TodoItem.js
+++ react/intoroduction/todo/src/TodoItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 function TodoItem(props) {
-    return (
-      <li>
-        {props.title}
-      </li>
-    )
-  }
+  return (
+    <li>
+      {props.title}
+    </li>
+  )
 }
 
 export default TodoItem;
