56699ad83540b2e2aa0ad64caea08a6f9740841a
Takuya <yky2800.rthm.55tn@gmail.com>
2021-09-12 21:26:36 +0900

    add redux-logger

--- react/intoroduction/redux-todo/src/index.js
+++ react/intoroduction/redux-todo/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { render } from 'react-dom';
 import tasksReduser from './reducers/tasks';
 import TodoApp from './containers/TodoApp';
+import { createLogger } from 'redux-logger'
+
+const loggerSetting = {
+  predicate: (getState, action) => action.type !== 'HIGH_FREQUENCY_ACTION'
+};
+
+const logger = createLogger(loggerSetting);
 
 // storeを生成
-const store = createStore(tasksReduser);
+const store = createStore(
+  tasksReduser,
+  applyMiddleware(logger)
+);
 
 render(
   <Provider store={store}>
