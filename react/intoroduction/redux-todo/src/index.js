241149897c7dfb44db185515975805681015a881
Takuya <yky2800.rthm.55tn@gmail.com>
2021-09-12 16:22:27 +0900

    dispatch, subscribe, getState, replaceResucer

--- react/intoroduction/redux-todo/src/index.js
+++ react/intoroduction/redux-todo/src/index.js
@@ -3,6 +3,66 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const initialState = {
+  tasks: []
+};
+
+// ActionCreator(Actionを生成するための関数)
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+function tasksReduser(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task]) // action.taskだと値が取れない
+      };
+    default:
+      return state;
+  }
+}
+
+function resetReducedr(state = initialState, action) {
+  switch (action.type) {
+    case 'RESET_TASK':
+      return {
+        ...state,
+        tasks: []
+      };
+    default:
+      return state;
+  }
+}
+
+// storeを生成
+const store = createStore(tasksReduser);
+
+function handleChange() {
+  console.log("handleChange() store.getState()", store.getState());
+}
+
+const unsubscribe = store.subscribe(handleChange)
+unsubscribe()
+
+store.dispatch(addTask('Storeを学ぶ'));
+console.log("store.getState()", store.getState());
+
+store.replaceReducer(resetReducedr);
+console.log("store.getState()", store.getState());
+
+const resetTask = () => ({
+  type: 'RESET_TASK'
+});
+
+store.dispatch(resetTask());
+console.log("After resetTask() store.getState()", store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
