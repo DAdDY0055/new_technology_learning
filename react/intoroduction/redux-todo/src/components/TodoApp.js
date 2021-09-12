9e02b2960681808504f6fed3d27a5e26bacf87c8
Takuya <yky2800.rthm.55tn@gmail.com>
2021-09-12 20:03:57 +0900

    不要なデバックを削除

--- react/intoroduction/redux-todo/src/components/TodoApp.js
+++ react/intoroduction/redux-todo/src/components/TodoApp.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default function TodoApp({ task, tasks, inputTask, addTask }) {
-  console.log("task", task)
-  console.log("tasks", tasks)
-  console.log("inputTask", inputTask)
-  console.log("addTask", addTask)
   return (
     <div>
       <input type="text" onChange={(e) => inputTask(e.target.value)} />
