dc13750cf920fd26b94c4c1b3a44e2dfc299d2d0
Takuya <yky2800.rthm.55tn@gmail.com>
2021-09-19 21:21:21 +0900

    ページのタイトルを追加

--- react/intoroduction/yahoo-shopping-ranking/src/App.js
+++ react/intoroduction/yahoo-shopping-ranking/src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Ranking from "./containers/Ranking";
 import Nav from "./containers//Nav";
+import Reboot from 'material-ui/Reboot'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import { Typography } from "material-ui";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Reboot />
+
+        <AppBar>
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Yahoo! ショッピングランキング
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
         <Nav />
 
         <Switch>
