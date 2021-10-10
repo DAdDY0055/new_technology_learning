c2bb38d19b255cade473b783ce69939f02c641df
Takuya <yky2800.rthm.55tn@gmail.com>
2021-10-10 09:32:20 +0900

    3-1 JavaScriptによるElementの取得

---
+++ react/Introduction_to_practice/chapter3/src/index.js
@@ -0,0 +1,16 @@
+console.log("test");
+
+const tilele1 = document.getElementById("title");
+console.log("title1", tilele1);
+
+const title2 = document.querySelector("#title");
+console.log("title2", title2)
+
+const containeres = document.getElementsByClassName("container");
+console.log("containeres", containeres)
+
+const containere = document.querySelector(".container");
+console.log("containere", containere)
+
+const containeres2 = document.querySelectorAll(".container");
+console.log("containeres2", containeres2)
