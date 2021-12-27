3d7a9deb0d3b18aa43c74df0268fe9a28eb8ce68
Takuya <yky2800.rthm.55tn@gmail.com>
2021-12-28 08:08:10 +0900

    4-4 イベントやスタイル

--- react/Introduction_to_practice/chapter4/my-app/src/App.jsx
+++ react/Introduction_to_practice/chapter4/my-app/src/App.jsx
@@ -1,13 +1,21 @@
 export const App = () => {
+  // ボタンを押した際にアラートを実行する関数
   const onClickButton = () => {
     alert("alert!");
   }
 
+  // CSSオブジェクト
+  const contentStyle = {
+    color: "blue",
+    fontSize: "200px"
+  };
+
   return (
     // 空タグで囲むとFragmentと同じ
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      {console.log("JSX内へのJavaScriptの記載")}
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   )
