4fb509466a05b1c819379b9923ab9c0a16ac91e5
Takuya <yky2800.rthm.55tn@gmail.com>
2022-01-01 16:12:49 +0900

    6-3 関数のmemo化

--- react/Introduction_to_practice/chapter6/memo/src/App.jsx
+++ react/Introduction_to_practice/chapter6/memo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from "react"
+import { useCallback, useState, memo } from "react"
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
 
@@ -11,13 +11,17 @@ export const App = memo(() => {
     setNum(num + 1);
   };
 
+  const onClickReset = useCallback(() => {
+    setNum(0);
+  }, []);
+
   return (
     <>
       <button onClick={onClickButton}>ボタンです</button>
       <p>{num}</p>
-      <Child1 />
+      {/* <Child1 /> */}
+      <Child1 onClickReset={onClickReset}/>
       <Child4 />
     </>
   );
 });
-
