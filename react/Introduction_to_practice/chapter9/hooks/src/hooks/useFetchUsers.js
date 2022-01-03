a0cc8528ce51190b2f95d326e78226f3c17383c4
Takuya <yky2800.rthm.55tn@gmail.com>
2022-01-03 10:31:20 +0900

    9-3 カスタムフックの利用

---
+++ react/Introduction_to_practice/chapter9/hooks/src/hooks/useFetchUsers.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import axios from "axios"
+
+// ユーザー一覧を取得するカスタムフック
+export const useFetchUsers = () => {
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  // const onClickFetchUser = () => alert('関数実行')
+  const onClickFetchUser = () => {
+    // ボタン押下時にローディングフラグon、エラーフラグoff
+    setIsLoading(true);
+    setIsError(false);
+
+    axios
+      .get("https://example.com/users")
+      .then(result => {
+        // 苗字と名前を結合するように変換
+        const users = result.data.map(user => ({
+          id: user.id,
+          name: `${user.lastname} ${user.firstname}`,
+          age: user.age
+        }));
+        // ユーザー一覧Stateを更新
+        setUserList(users);
+      })
+      // エラーの場合はエラーフラグをon
+      .catch(() => setIsError(true))
+      // 処理完了後はローディングフラグをoff
+      .finally(() => setIsLoading(false));
+  };
+
+  // オブジェクトとしてまとめて返却
+  return { userList, isLoading, isError, onClickFetchUser }
+}
