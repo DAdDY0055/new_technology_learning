5e260582113ceaf66aca805e3ae90a8272a9476d
Takuya <yky2800.rthm.55tn@gmail.com>
2022-03-06 10:46:17 +0900

    step0-2

--- javascript/step0/main.js
+++ javascript/step0/main.js
@@ -1,1 +1,17 @@
 console.log('ファイル読み込み');
+var element = document.getElementById('innerTest');
+element.innerHTML = '<strong>JavaScript</strong>で書きました'
+
+var buttonElm = document.getElementById('testButton');
+console.log('buttonElm', buttonElm);
+buttonElm.addEventListener('click', () => {
+  // alert('ボタンが押されました');
+  var numberElm = document.getElementById('number');
+  var val = numberElm.value;
+  var num = parseInt(val);
+  if (num % 2 == 0) {
+    alert('偶数です');
+  } else {
+    alert('偶数ではありません');
+  }
+});
