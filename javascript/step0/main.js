3a5116d03859e02968c3af9202eb8512ebfbc8b4
Takuya <yky2800.rthm.55tn@gmail.com>
2022-03-06 11:30:57 +0900

    step0-3

--- javascript/step0/main.js
+++ javascript/step0/main.js
@@ -3,7 +3,6 @@ var element = document.getElementById('innerTest');
 element.innerHTML = '<strong>JavaScript</strong>で書きました'
 
 var buttonElm = document.getElementById('testButton');
-console.log('buttonElm', buttonElm);
 buttonElm.addEventListener('click', () => {
   // alert('ボタンが押されました');
   var numberElm = document.getElementById('number');
@@ -15,3 +14,40 @@ buttonElm.addEventListener('click', () => {
     alert('偶数ではありません');
   }
 });
+
+var fruits = ['りんご', 'もも', 'みかん'];
+var fruitsStr = '';
+for (var i = 0; i < fruits.length; i++) {
+  fruitsStr += '<li class="fruit">' + fruits[i] + '</li>';
+}
+
+var arrayElm = document.getElementById('arrayTest');
+arrayElm.innerHTML = fruitsStr;
+
+// idと異なり、classは画面に複数配置することができるため、複数の要素が取れることがある
+var fruitElms = document.getElementsByClassName('fruit');
+console.log('fruitElms', fruitElms);
+console.log('fruitElms.length', fruitElms.length);
+for (var i = 0; i < fruitElms.length; i++) {
+  console.log('i', i);
+  var fruitElm = fruitElms[i];
+  console.log('fruitElm.textContent', fruitElm.textContent);
+}
+
+var colorsObj = {
+  red: 'あか',
+  grean: 'みどり',
+  blue: 'あお'
+};
+
+console.log('colorsObj', colorsObj);
+
+console.log('colorsObj', colorsObj['red']);
+console.log('colorsObj', colorsObj.red);
+console.log('colorsObj', colorsObj.blue);
+
+colorsObj['red'] = 'レッド';
+console.log('colorsObj', colorsObj.red);
+
+colorsObj.blue = 'ブルー';
+console.log('colorsObj', colorsObj.blue);
