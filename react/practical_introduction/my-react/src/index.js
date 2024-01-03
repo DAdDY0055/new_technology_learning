df52b92244ffe0969bf63450d040a0d53e647838
Takuya <yky2800.rthm.55tn@gmail.com>
2024-01-03 16:29:29 +0900

    use-immer

--- react/practical_introduction/my-react/src/index.js
+++ react/practical_introduction/my-react/src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 // import ListTemplate from './Children/ListTemplate';
 // import books from './contains/books';
 // import EventPoint from './Event/EventPoint';
-import StateForm from './Form/StateForm';
+import StateNestImmer from "./Form/StateNestImmer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // レンダープロップ
-  // <ListTemplate src={books} render={ elem => (
-  //   <>
-  //      <dt>
-  //        <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
-  //          {elem.title} ({elem.price}円)
-  //        </a>
-  //      </dt>
-  //   </>
-  // )}/>
+    // レンダープロップ
+    // <ListTemplate src={books} render={ elem => (
+    //   <>
+    //      <dt>
+    //        <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+    //          {elem.title} ({elem.price}円)
+    //        </a>
+    //      </dt>
+    //   </>
+    // )}/>
 
-  // <EventPoint />
-  <StateForm />
+    // <EventPoint />
+    <StateNestImmer />,
 );
 
 // If you want to start measuring performance in your app, pass a function
