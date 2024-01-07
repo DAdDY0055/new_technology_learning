b89a734d0acf91f79780478b7fb7bd6112ee7fe5
Takuya <yky2800.rthm.55tn@gmail.com>
2024-01-07 22:51:29 +0900

    doc: コメント追加

--- react/practical_introduction/my-react/src/hooks/HookTimer.js
+++ react/practical_introduction/my-react/src/hooks/HookTimer.js
@@ -8,7 +8,10 @@ export default function HookTimer({ init }) {
         const t = setInterval(() => {
             setCount((c) => c - 1);
         }, 1000);
+        // コンポーネント破棄時に return() が実行される
         return () => {
+            // setInterval() で呼び出されたタイマーを破棄
+            // https://developer.mozilla.org/docs/Web/API/clearInterval
             clearInterval(t);
         };
     });
